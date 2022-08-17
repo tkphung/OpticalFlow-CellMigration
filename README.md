c0cc59e1509d4fbbbb1938e571db4828ead4f047
T.K. Phung <tkphung@users.noreply.github.com>
2022-08-16 21:47:02 -0400

    Upload batch analysis code

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # Optical Flow for Cell Migration
+
 Use MATLAB's Computer Vision Toolbox to estimate trajectories of cells in a movie.
 
-# track_grid_example.m
-This script tracks motion of a grid of points seeded from the first frame using the built in MATLAB toolbox function `opticalFlowFarneback`. You specify the location of the grid by drawing a rectangle on the first frame. You can also specify the spacing of the grid by changing the value of `spaceout`.
+* `track_grid_example.m` tracks motion in one movie
+* `batch_analysis_example.m` batch processes a folder containing multiple movies (i.e. `test_movies`)
+
+
+## Getting Started
+
+### Dependencies
+
+* Cell segmentation code was developed on Windows 10 using MATLAB 2021a
+* MATLAB toolboxes required
+    * Image Processing Toolbox
+    * Computer Vision Toolbox
+* `batch_analysis_example.m` uses the [bio-formats library](https://github.com/microscopepony/bio-formats-matlab)
+
+
+### `track_grid_example.m`: process one movie
+
+`track_grid_example.m` tracks motion of a grid of points seeded from the first frame using the built in MATLAB toolbox function `opticalFlowFarneback`. You specify the location of the grid by drawing a rectangle on the first frame. You can also specify the spacing of the grid by changing the value of `spaceout`.
+
+
+### `test_movies`: dataset
+
+Sample movies for cell migration analysis.
+
+* Humnan bronchial airway epithelial cells grown in Air-Liquid Interface culture
+* Cells are imaged using time-lapse, phase-contrast microscopy (6 min between frames)
+
+### `batch_analysis_example.m`: batch process movies
+
+`batch_analysis_example.m` processes all movies within a folder (i.e. `test_movies`). This code automatically sets the tracking grid based on distance from the field of view border. The final block of code visualizes the migration paths from the first movie in the folder.
+
+## Authors
+
+Thien-Khoi N. Phung [@tkphung](https://twitter.com/tkphung)
+
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [Optical Flow Farneback Method (MATLAB)](https://www.mathworks.com/help/vision/ref/opticalflowfarneback.html)
---
+++ batch_analysis_example.m
@@ -0,0 +1,101 @@
+% batch_analysis_example.m
+% This script takes in the time lapse movies and estimates the cell
+% migration speeds. 
+% We use the library "bio-formats-matlab" to load movies
+% (github.com/microscopepony/bio-formats-matlab)
+%
+% Thien-Khoi N. Phung (February 22, 2021)
+
+%% Grab all of the images
+allimgs = dir('test_movies\*.tiff');
+
+%% Process Speeds
+for jz = 1:numel(allimgs)
+    
+    % Filename
+    imgname = [allimgs(jz).folder '\' allimgs(jz).name];
+    
+    % Time Lapse ID [Date Capture#]
+    tlid{jz} = [allimgs(jz).name(1:9),...
+                cell2mat(extractBetween(allimgs(jz).name,'s','.ome'))];
+    
+    % Load in image file   
+    data = bfopen(imgname);
+    
+    % Pixel Resolution
+    OMEdata = data{1, 4};
+    voxelSizeX = OMEdata.getPixelsPhysicalSizeX(0).value(ome.units.UNITS.MICROMETER); % in µm
+    voxelSizeY = OMEdata.getPixelsPhysicalSizeY(0).value(ome.units.UNITS.MICROMETER); % in µm
+    pixres = [voxelSizeX.doubleValue() voxelSizeY.doubleValue()]; %(um/pixel)
+    
+    % Make a video from image sequence
+    for ss = 1:size(data{1},1)
+        tlstack(:,:,ss) = data{1}{ss,1};
+    end
+    
+    % Generate coordinates for all pixels
+    [r,c]     = size(tlstack(:,:,1));
+    [xg,yg]   = meshgrid(1:c,1:r);
+    
+    % Downsample Grid
+    spaceout  = round(10/pixres(1)); % Spacing of grid (pixels)- 10 um
+    [xgd,ygd] = meshgrid(1:spaceout:c,1:spaceout:r);
+    xo = xgd(:);
+    yo = ygd(:);
+    
+    % Remove grid points near border
+    border    = round(50/pixres(1)); % 50 um -> pixels
+    brectx    = [border c-border];
+    brecty    = [border r-border];
+    inborder  = inpolygon(xo,yo,brectx,brecty);
+    xx        = xo(inborder);
+    yy        = yo(inborder);
+    
+    % Setup optical flow
+    opticFlow = opticalFlowFarneback;
+    dt        = 1; % Time step (Frame rate)
+    
+    % Calculate flow
+    for frame = 1:size(tlstack,3)
+        disp(['Flow Calculation Frame ' num2str(frame)])
+        
+        % Grab frame
+        imframe = im2uint8(tlstack(:,:,frame));
+        
+        % estimate flow
+        flow  = estimateFlow(opticFlow,imframe);
+        
+        if frame>1
+        % Calculate the velocities at the seed points [xx,yy]
+        vxint = interp2(xg,yg,double(flow.Vx),xx(:,frame-1),yy(:,frame-1),'spline');
+        vyint = interp2(xg,yg,double(flow.Vy),xx(:,frame-1),yy(:,frame-1),'spline');
+
+        % Calculate the new position of the points in the next frame
+        xx(:,frame) = xx(:,frame-1) + vxint.*dt;
+        yy(:,frame) = yy(:,frame-1) + vyint.*dt;
+        end
+    end
+    
+    % Estimate migration velocity
+    dist = sqrt(diff(xx,[],2).^2 + diff(yy,[],2).^2).*pixres(1); % um
+    msd  = mean(dist,2); % um/frame
+    velo = msd./(3/60); % um/hour
+   
+    % Store data
+    tldata{jz}.xx   = xx;
+    tldata{jz}.yy   = yy;
+    tldata{jz}.msd  = msd;
+    tldata{jz}.velo = velo;
+    tldata{jz}.file = imgname;
+    
+    clear tlstack
+end
+
+%% Visualize tracking
+% Which movie to visualize
+imid = 1;
+
+figure
+plot(tldata{imid}.xx',tldata{imid}.yy','k-')
+axis ij equal tight
+title(tldata{imid}.file)
